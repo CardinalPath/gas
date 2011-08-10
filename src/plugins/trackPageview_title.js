a28dbef303245006fd634daa8b1c0e171777fc65
Eduardo Cereto <eduardocereto@gmail.com>
2011-08-10 06:08:04 -0300

    New plugin to change the page title for _trackPageviews

---
+++ src/plugins/trackPageview_title.js
@@ -0,0 +1,19 @@
+/**
+ * Enables setting of page Title on _trackPageview.
+ *
+ * This Hook cancels the execution of the current pageview and fires a new one.
+ * for this reason this hook must be inserted early on the hook list,
+ * so other hooks don't fire twice.
+ */
+_gas.push(['_addHook', '_trackPageview', function(url, title) {
+    if (title && typeof title === 'string') {
+        var oTitle = document.title;
+        _gas.push(
+            function() {document.title = title;},
+            ['_trackPageview', url],
+            function() {document.title = oTitle;}
+        );
+        return false;
+    }
+    return [url];
+}]);
