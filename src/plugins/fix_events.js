b11ea8a5ae8d0590b23cfac0060f01bb284b3ba0
Eduardo Cereto <eduardocereto@gmail.com>
2011-09-06 05:06:09 -0300

    Fire negative valued events as val=0

--- src/plugins/fix_events.js
+++ src/plugins/fix_events.js
@@ -2,10 +2,12 @@
  * Hook to sanity check trackEvents
  *
  * The value is rounded and parsed to integer.
+ * Negative values are sent as zero.
+ * If val is NaN than it is sent as zero.
  */
 window._gas.push(['_addHook', '_trackEvent', function(cat, act, lab, val) {
     if (val) {
-        val = Math.abs(Math.round(val)) || 0;
+        val = (val < 0 ? 0 : Math.round(val)) || 0;
     }
     return [cat, act, lab, val];
 }]);
