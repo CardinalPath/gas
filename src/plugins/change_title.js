a7a5871a48cf06333d427ed3146e06873a734a05
Eduardo Cereto <eduardocereto@gmail.com>
2012-02-15 02:28:17 -0800

    Pass on all arguments to avoid future problems, if more parameters are added to _trackPageview

--- src/plugins/change_title.js
+++ src/plugins/change_title.js
@@ -2,14 +2,16 @@
  * This is kept just for backward compatibility since it's now supported
  * natively in _gaq.
  */
-_gas.push(['_addHook', '_trackPageview', function(url, title) {
-    var obj = url;
-    if (typeof url === 'string' && typeof title === 'string') {
-        obj = {
-            'page': url,
-            'title': title
-        };
+_gas.push(['_addHook', '_trackPageview', function() {
+    var args = slice.call(arguments);
+    if (args.length >= 2 &&
+        typeof args[0] === 'string' && typeof args[1] === 'string')
+    {
+        return [{
+            'page': args[0],
+            'title': args[1]
+        }];
     }
-    return [obj];
+    return args;
 }]);
 
