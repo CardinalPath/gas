ff71e76f2f79096e1e095ae4ae4654942426ead3
Eduardo Cereto <eduardocereto@gmail.com>
2012-04-10 16:47:42 -0700

    Use sindexOf on mailto plugin

--- src/core.js
+++ src/core.js
@@ -39,7 +39,7 @@ var document = window.document,
  */
 function GAS() {
     var self = this;
-    self['version'] = '1.6';
+    self['version'] = '1.7';
     self._accounts = {};
     self._accounts_length = 0;
     self._queue = _prev_gas;
